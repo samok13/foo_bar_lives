750a7153bb511b0831ea98726d4947c838539157
Sam O'Keefe <samok13@gmail.com>
2016-03-14 09:15:22 -0700

    foo bar lives assessment

--- comb-sort/combSort.js
+++ comb-sort/combSort.js
@@ -1,6 +1,27 @@
 
 
 var combSort = function(array) {
-  // Your code here
+
+  var gap = array.length;
+  var shrink = 1.3;
+  var swapped;
+
+  while(gap > 1 || swapped === true){
+    gap = parseInt(gap/shrink);
+    if(gap < 1){
+      gap = 1;
+    }
+
+   swapped = false;
+    for(var i = 0; i+ gap < array.length; i++){
+      if( array [i] > array [i + gap]){
+        var temp = array[i];
+        array[i] = array[i+gap];
+        array[i+gap] = temp;
+        swapped = true;
+      }
+    }    
+  }
+  return array;   
 };
 
--- groceries/groceries.js
+++ groceries/groceries.js
@@ -1,2 +1,35 @@
 // Your Code Here!
 
+// ### Features:
+
+// 1. Add a grocery item via a form
+// 1. Remove a grocery item via a delete button next to the item in the list
+// 1. If the value submitted by the form is empty, do NOT create an item in the list
+
+//form
+
+
+
+
+function addItem(item){
+  var $listitem = $('<li>' + item + '</li>')
+  var $button = $('<button>Delete</button>')
+  $listitem.append($button);
+  $('#list').append($listitem);
+
+  $button.on("click", function(event){
+    $listitem.remove();
+  })
+
+}
+
+$('form').on('submit', function(event){
+  event.preventDefault();
+  var item = $('#item').val();
+
+  if(item){
+    addItem(item);
+  }
+});
+
+
